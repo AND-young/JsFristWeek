538b6c970facea45d904cd46f2e1000991ec3aac
young <844186566@qq.com>
2017-11-23 21:21:40 +0800

    商品排序的实现

--- js/Ajax.js
+++ js/Ajax.js
@@ -49,6 +49,7 @@
     var ListBox = document.getElementById('list');
     for (var i = 0; i < result.length; i++) {
         var item = result[i];
+        item.time = item.time.replace(/-/g, '');
         str += `<li data-price="${item.price}" data-hot = "${item.hot}" data-time = "${item.time}">
             <a href="javascript:;">
             <img src="${item.img}" alt="">
@@ -62,39 +63,61 @@
 ~function () {
     var ListBox = document.getElementById('list');
     var oLis = ListBox.children;
-    oLis = utils.toArray(oLis);
-
-
     var header = document.getElementById('header');
-    var a = header.getElementsByTagName('a');
+    var btn = header.getElementsByTagName('a');
+    var arr = ['data-time', 'data-price', 'data-hot'];
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].myMethod = -1;
+        btn[i].index = i;
+        btn[i].onclick = function () {
+            this.myMethod*=-1;
+            changeP(arr[this.index], this.myMethod, this.index);
+        }
+    }
 
-    a[1].onclick = function () {
-        oLis.sort(function (a, b) {
-            var Price = a.getAttribute('data-price') - b.getAttribute('data-price');
-            return Price;
-        });
-        for (var i = 0; i < oLis.length; i++) {
-            ListBox.appendChild(oLis[i]);
+    function changeP(data, who, index) {
+        for (var i = 0; i < btn.length; i++) {
+            if (i != index) {
+                btn[i].myMethod = -1;
+            }
         }
-    };
-    a[2].onclick = function () {
+        oLis = utils.toArray(oLis);
         oLis.sort(function (a, b) {
-            var hot = b.getAttribute('data-hot') - a.getAttribute('data-hot');
-            return hot;
+            var hot = a.getAttribute(data) - b.getAttribute(data);
+            return hot * who;
         });
+        var frg = document.createDocumentFragment();
         for (var i = 0; i < oLis.length; i++) {
-            ListBox.appendChild(oLis[i]);
+            frg.appendChild(oLis[i]);
         }
+        ListBox.appendChild(frg);
+        frg = null;
     }
-    a[0].onclick = function () {
-        oLis.sort(function (a, b) {
-            var time = new Date(b.getAttribute('data-time')) - new Date(a.getAttribute('data-time'));
-            return time;
-        });
-        for (var i = 0; i < oLis.length; i++) {
-            ListBox.appendChild(oLis[i]);
-        }
-    }
+
+
+    // btn[1].myMethod = -1;
+    // btn[2].myMethod = -1;
+    // btn[1].onclick = function () {
+    //     this.myMethod*=-1;
+    //     changeP('price',this.myMethod);
+    // };
+    // btn[2].onclick = function () {
+    //     this.myMethod*=-1;
+    //     changeP('hot',this.myMethod);
+    // };
+    // btn[0].onclick = function () {
+    //     oLis.sort(function (a, b) {
+    //         // var time = new Date(b.getAttribute('data-time')) - new Date(a.getAttribute('data-time'));
+    //         var time = b.getAttribute('data-time') - a.getAttribute('data-time');
+    //         return time;
+    //     });
+    //     var frg = document.createDocumentFragment();
+    //     for (var i = 0; i < oLis.length; i++) {
+    //         frg.appendChild(oLis[i]);
+    //     }
+    //     ListBox.appendChild(frg);
+    //     frg = null;
+    // }
 }();
 
 
